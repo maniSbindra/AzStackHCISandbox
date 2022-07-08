85dba79092daa97ae3f25955b548ae50e75c06b5
Maninderjit Bindra <maninder.bindra@gmail.com>
2022-07-08 14:55:23 +0530

    Add ipv4 dns address

--- Sandbox/New-AzSHCISandbox.ps1
+++ Sandbox/New-AzSHCISandbox.ps1
@@ -3428,6 +3428,8 @@ if (!$SDNConfig.MultipleHyperVHosts) {
     $ifIndex = (Get-NetAdapter | ? {$_.name -like "*$switchName)"}).ifIndex
     New-NetIPAddress -IPAddress 192.168.0.1 -InterfaceIndex $ifIndex -PrefixLength 24
 
+    Set-DnsClientServerAddress -InterfaceAlias 'vEthernet (newintswitch)' -ServerAddresses ("168.63.129.16")
+
     Add-DhcpServerV4Scope -Name "DHCP-$switchName" -StartRange 192.168.0.50 -EndRange 192.168.0.100 -SubnetMask 255.255.255.0
     Set-DhcpServerV4OptionValue -Router 192.168.0.1 -DnsServer 168.63.129.16
     Restart-service dhcpserver
